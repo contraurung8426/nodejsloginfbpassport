f133c4d3d776fc6ac70e40a3661e2b395aa85ee8
Yasuo <contraurung8426@gmail.com>
2020-06-04 15:40:19 +0700

    haha

--- models/FbUser.js
+++ models/FbUser.js
@@ -3,18 +3,15 @@ const mongoose = require('mongoose');
 const UserFbSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
     },
     email: {
         type: String
     },
     facebookID: {
         type: String,
-        required: true
     },
     accessToken: {
         type: String,
-        required: true
     },
     date: {
         type: Date,
