400d692a45ee861bb844beeacba701acf7901bdb
Yasuo <contraurung8426@gmail.com>
2020-06-04 14:22:03 +0700

    fixing

--- models/User.js
+++ models/User.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: false
-    },
     name: {
         type: String,
         required: true
