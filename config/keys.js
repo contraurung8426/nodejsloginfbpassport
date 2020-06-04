3fe877ac1893163ca470a7e1fd315cb8a759c629
Yasuo <contraurung8426@gmail.com>
2020-06-04 09:47:26 +0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://yasuo:123@test-luster-1-5ez3f.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ config/passport_fb.js
@@ -0,0 +1,28 @@
+const passportfb = require('passport-facebook').Strategy;
+
+// Load User Model
+const User = require('../models/User');
+
+module.exports = function(passport){
+  passport.use(
+    new passportfb({
+      clientID: "178464433586069",
+      clientSecret: "062cd4c72ccbd545e59f106dd76741a9",
+      callbackURL: "http://localhost:3000/login_fb/cb"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+    }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done)=> {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+}
